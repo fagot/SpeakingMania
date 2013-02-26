f96197b95df89fec86eaec307ae89c21d830d33f
Petro Lyzhny <petro.luzhny@perfectial.com>
2013-02-26 18:48:57 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SpeakingMania.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Room()
+        {
+            return View("Room");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+BootstrapMVC is an ASP.NET MVC project using Twitter Bootstrap as its theme. 
+To use it download the project file from the github repository and open the solution in Visual Studio.NET 2010
+
+project page: http://www.littlebigtomatoes.com/bootstrapmvc/
+
+screenshot
+
+![BootstrapMVC](http://www.littlebigtomatoes.com/images/BootstrapMVC-300x217.png)
